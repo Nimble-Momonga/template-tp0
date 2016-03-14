cfa68e772671b37e23a5730d2ceff998d74577d7
mtejedor <mtejedor@hexacta.com>
2016-03-14 16:18:19 -0300

    deleted RegExToken hierarchy

--- src/main/java/ar/fiuba/tdd/template/tp0/RegExToken.java
+++ src/main/java/ar/fiuba/tdd/template/tp0/RegExToken.java
@@ -1,14 +1,19 @@
 package ar.fiuba.tdd.template.tp0;
 
+import java.util.Random;
+
 /**
  * Created by manuelcruz on 11/03/2016.
  */
-abstract class RegExToken {
-    protected String token;
-    protected RegExQuantifier quantifier;
+public class RegExToken {
+    private static Random RANDOM = new Random();
+    private Boolean isLiteral;
+    private String token;
+    private RegExQuantifier quantifier;
 
-    public RegExToken(String token) {
+    public RegExToken(String token, Boolean literal) {
         this.token = token;
+        this.isLiteral = literal;
         this.quantifier = new RegExQuantifier();
     }
 
@@ -16,6 +21,46 @@ abstract class RegExToken {
         this.quantifier = quantifier;
     }
 
-    abstract String generate();
+    private String generateLiteral() {
+        int quantity = this.quantifier.generate();
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int i = 0; i < quantity; i++) {
+            stringBuilder.append(this.token);
+        }
+        return stringBuilder.toString();
+    }
+
+    private String generateDot() {
+        int quantity = this.quantifier.generate();
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int i = 0; i < quantity; i++) {
+            stringBuilder.append(RANDOM.nextInt(256));
+        }
+        return stringBuilder.toString();
+    }
+
+    private String generateSet(int tokenLength) {
+        int quantity = this.quantifier.generate();
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int i = 0; i < quantity; i++) {
+            stringBuilder.append(this.token.charAt(RANDOM.nextInt(tokenLength)));
+        }
+        return stringBuilder.toString();
+    }
+
+    public String generate() {
+        String generated;
+        int tokenLength = this.token.length();
+        if (tokenLength > 1) {
+            generated = generateSet(tokenLength);
+        } else {
+            if (this.token.equals("*") && !this.isLiteral) {
+                generated = generateDot();
+            } else {
+                generated = generateLiteral();
+            }
+        }
+        return generated;
+    }
 
 }
